4ca20b1ee7f354d0b2ef096bd34d5db1ba112557
ningsheng <ningsheng@inspur.com>
2017-05-16 19:59:19 +0800

    docker

--- cloud-ops-esc/src/main/java/com/cloud/ops/esc/docker/DockerLauncher.java
+++ cloud-ops-esc/src/main/java/com/cloud/ops/esc/docker/DockerLauncher.java
@@ -16,33 +16,35 @@ import java.util.Properties;
 import java.util.Set;
 
 public class DockerLauncher {
+    private static final String PATH = "E:\\iop\\cloud-ops\\cloud-ops-esc\\src\\main\\resources\\";
     // get a context with docker that offers the portable ComputeService api
     public static void main(String[] args) throws RunNodesException {
 
-        String email = "E:\\iop\\cloud-ops\\cloud-ops-esc\\src\\main\\java\\com\\cloud\\ops\\esc\\docker\\cert.pem";
-        String password = "E:\\iop\\cloud-ops\\cloud-ops-esc\\src\\main\\java\\com\\cloud\\ops\\esc\\docker\\key.pem";
+        String email = "cert.pem";
+        String password = "key.pem";
 
         // get a context with docker that offers the portable ComputeService api
         ContextBuilder contextBuilder = ContextBuilder.newBuilder("docker").endpoint("https://10.110.19.226:2376")
-                .credentials(email, password)
+                .credentials(PATH + email, PATH + password)
                 .modules(ImmutableSet.<Module>of(new Log4JLoggingModule(),
                         new SshjSshClientModule()));
 
         Properties props = new Properties();
 
-        props.setProperty("docker.cacert.path","E:\\iop\\cloud-ops\\cloud-ops-esc\\src\\main\\java\\com\\cloud\\ops\\esc\\docker\\ca.pem");
+        props.setProperty("docker.cacert.path", PATH + "ca.pem");
         contextBuilder.overrides(props);
 
         ComputeServiceContext context = contextBuilder.buildView(ComputeServiceContext.class);
         ComputeService client = context.getComputeService();
-        System.out.println(client.listImages());;
-//        String sshableImageId = "48b5124b2768d2b917edcb640435044a97967015485e812545546cbed5cf0233"; // this can be obtained using `docker images --no-trunc` command
+        System.out.println(client.listImages());
+//        this can be obtained using `docker images --no-trunc` command
+//        String sshableImageId = "48b5124b2768d2b917edcb640435044a97967015485e812545546cbed5cf0233";
 //        Template template = client.templateBuilder().imageId(sshableImageId).build();
 
-// run a couple nodes accessible via group container
+//        run a couple nodes accessible via group container
 //        Set<? extends NodeMetadata> nodes = client.createNodesInGroup("container", 2, template);
 
-// release resources
+//        release resources
         context.close();
 
     }
